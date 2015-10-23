d914688ff81e631680376a8a08bafcc1cbf280b0
aidewoode <aidewoode@gmail.com>
2015-10-23 23:22:33 +0800

    add a new blog

--- assets/components/article-preview.vue
+++ assets/components/article-preview.vue
@@ -1,7 +1,6 @@
 <template lang='jade'>
   li.articles-list__item
-    .articles-list__date
-      {{article.date | formattedDate}}
+    .articles-list__date {{article.date | formattedDate}}
     .articles-list__title
       a.articles-list__link(v-link="{ name: 'article', params: { id: article.id }}") {{article.title}}
 </template>
