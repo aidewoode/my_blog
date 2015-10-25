cd3e1a2a4b36e875ca407eef46a168585a503365
aidewoode <aidewoode@gmail.com>
2015-10-25 11:09:04 +0800

    remove some unnecessary style

--- assets/app.vue
+++ assets/app.vue
@@ -1,8 +1,8 @@
 <template lang='jade'>
 .site-content
-  nav.global-nav.clearfix
-    a.global-nav__title.float-left(v-link="{name: 'home'}") Aide's Blog
-    ul.global-nav__list.float-left
+  nav.global-nav
+    a.global-nav__title(v-link="{name: 'home'}") Aide's Blog
+    ul.global-nav__list
       li.global-nav__item
         a.global-nav__link(href='https://github.com/aidewoode' target='_blank')
           i.icon.icon--github
