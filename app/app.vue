094ec1be6e2e65f1cb068aeb664314621ba49f28
aidewoode <aidewoode@gmail.com>
2017-04-30 16:07:51 +0800

    add browser update info

--- app/app.vue
+++ app/app.vue
@@ -9,14 +9,12 @@
       li.global-nav__item
         a.global-nav__link(href='mailto:aidewoode@gmail.com')
           i.icon.icon--mail
-      li.global-nav__item
-        a.global-nav__link(href='#')
-          i.icon.icon--rss
     loader(v-if='loading')
   .content-container
     router-view
 </template>
 <script>
+  import browserUpdate from 'browser-update';
   import loader from './components/loader.vue';
   import mixin from './mixin';
 
@@ -30,6 +28,25 @@
     created() {
       this.loadingBus.$on('toggleLoading', (val) => {
         this.loading = val;
+      });
+    },
+
+    mounted() {
+      browserUpdate({
+        vs: { i: 11, f: 50, o: 17, s: 10, c: 50 },
+        text_en: `
+          Your browser, {brow_name}, is too old.
+          This site maybe have some display issues on your browser,
+          please update your browser
+          <a{up_but}>update</a><a{ignore_but}>ignore</a>
+        `,
+        text_zh: `
+          你的浏览器 {brow_name} 太过老旧了，
+          本网站在此浏览器上可能会有显示问题，
+          请升级你的浏览器
+          <a{up_but}>升级</a><a{ignore_but}>忽略</a>
+        `,
+        test: true
       });
     },
 
